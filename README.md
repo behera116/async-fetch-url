7cce2b6330287d3007b7905da427e56825f83ae1
behera116 <ananda@mysmartprice.com>
2019-12-23 19:04:46 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # async-fetch-url
 async/await implementation of fetchUrl function of fetch package.
+
+## Install
+
+    npm install async-fetch-url
+
+## Basic implementation
+
+```js
+const fetchUrl = require('async-fetch-url').asyncFetchUrl;
+( async() => {
+    var url = "https://www.thehindu.com/";
+    var response = await fetchUrl(url);
+    console.log(JSON.stringify(response));
+
+})();
+```
+
+Output format:
+
+```javascript
+{
+    'error': Object,//error.message to get error info
+    'meta': Object,
+    'data': String //html content of the page
+}
+
+```
+---
---
+++ index.js
@@ -0,0 +1,46 @@
+const fetch = require('fetch');
+const fetchUrl = fetch.fetchUrl;
+
+const asyncFetchUrl = async(url,options,callback) => {
+    
+    try{
+        if (!callback && typeof options === 'function') {
+            callback = options;
+            options = undefined;
+        }
+
+        options = options || {};
+        return new Promise( (resolve,reject) => {
+            fetchUrl(url,options,(error,meta,data) => {
+                data = typeof data === 'undefined'?undefined:data.toString();
+                resolve({
+                    'error': error,
+                    'meta': meta,
+                    'data': data
+                });
+            });
+        });
+
+    }catch(err){
+        return new Promise( (resolve,reject) => {
+            reject({
+                'error': err,
+                'meta': undefined,
+                'data': undefined
+            });
+        });
+    }
+   
+}
+
+/*( async() => {
+    var url = "https://www.npmjs.com/package/page-dweller";
+    var response = await asyncFetchUrl(url);
+    console.log(response);
+})();*/
+
+
+module.exports = {
+    'asyncFetchUrl':asyncFetchUrl
+}
+
